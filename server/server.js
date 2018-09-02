5fe45145e751a4a591a5a6edc36926d0974540a7
fwahlberg <wahlbergf100@gmail.com>
2018-09-02 18:35:56 +0100

    Validators, Types and Defaults

--- server/server.js
+++ server/server.js
@@ -8,13 +8,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 //Todo Model
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
@@ -26,4 +31,25 @@ newTodo.save().then((doc) =>{
   console.log('Saved todo', doc)
 }, (e) => {
   console.log('Unable to save todo')
+});
+
+
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+var newUser = new User({
+  email: "wahlbergf@msn.com"
+});
+
+newUser.save().then((doc) => {
+  console.log('Saved User', doc);
+}, (e) => {
+  console.log('Unable to save new user');
 });
