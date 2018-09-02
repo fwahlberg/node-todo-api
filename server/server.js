3d10c74be9671500fd59097311a2dc9b9edf717c
fwahlberg <wahlbergf100@gmail.com>
2018-09-02 19:53:28 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+  mongoose
+} = require('./db/mongoose');
+var {
+  Todo
+} = require('./models/todo');
+var {
+  User
+} = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
-app.post('/todos', (req, res) =>{
+app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
@@ -20,8 +26,20 @@ app.post('/todos', (req, res) =>{
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    })
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
-module.exports = {app};
+module.exports = {
+  app
+};
