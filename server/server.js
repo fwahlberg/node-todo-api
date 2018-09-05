2aeea9892ed437c42d14c19deaab01e7bbe7bf23
fwahlberg <wahlbergf100@gmail.com>
2018-09-05 21:12:01 +0100

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -131,12 +131,13 @@ app.post('/users', (req, res) => {
   //Allow users to only modify email and password
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
-
-  user.save().then((user) => {
-    res.send(user);
-  }, (e) => {
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+}).catch((e) => {
     res.status(400).send(e);
-  });
+  })
 });
 
 app.listen(port, () => {
