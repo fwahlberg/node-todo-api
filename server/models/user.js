a69b159245bfc7d2fb6d0f137770121b52c47ad7
fwahlberg <wahlbergf100@gmail.com>
2018-09-06 20:38:30 +0100

    Add /GET /users/me and auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -12,44 +12,69 @@ var UserSchema = new mongoose.Schema({
     unique: true,
     validate: {
       validator: validator.isEmail,
-    messgae: `{VALUE} is not a valid email`
-  }
-},
-password: {
-  type: String,
-  require: true,
-  minlength: 6
-},
-tokens: [{
-  access: {
-    type: String,
-    required: true
+      messgae: `{VALUE} is not a valid email`
+    }
   },
-  token: {
+  password: {
     type: String,
-    required: true
-  }
-}]
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
-UserSchema.methods.toJSON = function () {
+UserSchema.methods.toJSON = function() {
   var user = this;
   var userObject = user.toObject();
   //return userObject;
   return _.pick(userObject, ['_id', 'email']);
 };
 
-UserSchema.methods.generateAuthToken = function () {
+UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({
+    _id: user._id.toHexString(),
+    access
+  }, 'abc123').toString();
 
-  user.tokens = user.tokens.concat([{access, token}]);
+  user.tokens = user.tokens.concat([{
+    access,
+    token
+  }]);
 
   return user.save().then(() => {
     return token;
   });
 };
+
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ var {
 var {
   User
 } = require('./models/user');
+var {
+  authenticate
+} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -135,9 +138,15 @@ app.post('/users', (req, res) => {
     return user.generateAuthToken();
   }).then((token) => {
     res.header('x-auth', token).send(user);
-}).catch((e) => {
+  }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
