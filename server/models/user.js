01cbb7399e0bd8cece83c3caeaf75681581248dc
fwahlberg <wahlbergf100@gmail.com>
2018-09-08 23:26:59 +0100

    //User

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,17 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: {
+        token
+      }
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
@@ -76,17 +87,19 @@ UserSchema.statics.findByToken = function(token) {
 
 };
 
-UserSchema.statics.findByCredentials = function (email, password) {
+UserSchema.statics.findByCredentials = function(email, password) {
   var User = this;
 
-  return User.findOne({email}).then((user) => {
-    if(!user) {
+  return User.findOne({
+    email
+  }).then((user) => {
+    if (!user) {
       return Promise.reject();
     }
 
     return new Promise((resolve, reject) => {
       bcrypt.compare(password, user.password, (err, res) => {
-        if(res) {
+        if (res) {
           resolve(user);
         } else {
           reject();
@@ -96,7 +109,7 @@ UserSchema.statics.findByCredentials = function (email, password) {
   });
 };
 
-UserSchema.pre('save', function (next) {
+UserSchema.pre('save', function(next) {
   var user = this;
 
   if (user.isModified('password')) {
